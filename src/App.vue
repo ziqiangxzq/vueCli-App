3b90446558a713df398d403ff20ed92028884a5d
ziqiangxzq <ziqiangxzq@163.com>
2019-04-05 12:12:25 +0800

    appadd

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    
+    <HelloWorld />
+    <SlideShow />
+    <ZhuTi />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SlideShow from  './components/SlideShow.vue'
+import ZhuTi from './components/ZhuTi.vue'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    SlideShow,
+    ZhuTi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
